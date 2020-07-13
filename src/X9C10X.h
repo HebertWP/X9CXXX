e4c51a92281620589c69a2a087f8964a92d4c48c
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-07-13 18:17:26 -0300

    typo corection in API

--- src/X9C10X.h
+++ src/X9C10X.h
@@ -1,5 +1,6 @@
 #ifndef __X9C10X__
 #define __X9C10X__
+
 #include <Arduino.h>
 
 class X9C10X
@@ -10,12 +11,13 @@ private:
     uint8_t up_dow__;
 public:
     X9C10X(uint8_t cs, uint8_t inc, uint8_t up_dow);
-    void wiperUp(uint8_t spep=1);
-    void wiperDow(uint8_t spep=1);
+    void begin();
+    void wiperUp(uint8_t step=1);
+    void wiperDow(uint8_t step=1);
     void toStandby(uint8_t storeWiper=false);
     bool isInStandby();
 
 private:
-}
+};
 
 #endif
