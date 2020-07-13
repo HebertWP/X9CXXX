5ec7355912096e92aea735aeb99a495da3c64200
Hebert Wandick Parreira <42666810+HebertWP@users.noreply.github.com>
2020-07-13 15:29:02 -0300

    API redesign

--- src/X9C10X.h
+++ src/X9C10X.h
@@ -10,11 +10,12 @@ private:
     uint8_t up_dow__;
 public:
     X9C10X(uint8_t cs, uint8_t inc, uint8_t up_dow);
-    void wiperUp(bool store = true);
-    void wiperDow(bool store = true);
+    void wiperUp(uint8_t spep=1);
+    void wiperDow(uint8_t spep=1);
+    void toStandby(uint8_t storeWiper=false);
+    bool isInStandby();
 
 private:
-    void storeWiperPosition(bool store = true);
 }
 
 #endif
